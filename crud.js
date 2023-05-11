16e7f6b9e4780d74a8c143af55a04c815e526fb3
William Huang <107070711+imwilliamhuang@users.noreply.github.com>
2023-05-11 18:24:02 -0300

    Update crud.js

--- crud.js
+++ crud.js
@@ -1,65 +1,67 @@
-document.querySelector("#salvar").addEventListener("click", cadastrar)
+document.querySelector("#salvar").addEventListener("click", cadastrar);
 
-function cadastrar(){
-    const titulo = document.querySelector("#titulo").value
-    const descricao = document.querySelector("#descricao").value
-    const categoria = document.querySelector("#categoria").value
-    const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
+function cadastrar() {
+  const titulo = document.querySelector("#titulo").value;
+  const descricao = document.querySelector("#descricao").value;
+  const categoria = document.querySelector("#categoria").value;
+  const modal = bootstrap.Modal.getInstance(document.querySelector("#exampleModal"))
 
-    const tarefa = {
-        titulo,
-        descricao,
-        categoria,
-    }
+  const tarefa = {
+    titulo,
+    descricao,
+    categoria,
+  }
 
-    if (!validar(tarefa.titulo, document.querySelector("#titulo"))) return
-    if (!validar(tarefa.descricao, document.querySelector("#descricao"))) return
+  if(!validar(tarefa.titulo, document.querySelector("#titulo"))) return
+  if(!validar(tarefa.descricao, document.querySelector("#descricao"))) return
+  
+  document.querySelector("#tarefas").innerHTML += createCard(tarefa)
 
-    document.querySelector("#tarefas").innerHTML += createCard(tarefa)
+  modal.hide()
 
-    modal.hide()
+}
 
 
-function validar(valor,campo){
+function validar(valor, campo){
     if(valor == ""){
         campo.classList.add("is-invalid")
         campo.classList.remove("is-valid")
         return false
     }
-
+    
     campo.classList.remove("is-invalid")
     campo.classList.add("is-valid")
     return true
-
-}    
+}
 
 function apagar(botao){
     botao.parentNode.parentNode.parentNode.remove()
 }
 
-function createCard(titulo, descricao){
-    return `
-                    <div class="col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-header">
-                            ${tarefa.titulo}
-                            </div>
-                            <div class="card-body">
-                            <p class="card-text">${tarefa.descricao}</p>
-                            <p>
-                                <span class="badge text-bg-warning">${tarefa.categoria}</span>
-                            </p>
-                            <a href="#" class="btn btn-success" title="Marcar como Concluída">
-                                <i class="bi bi-check-lg"></i>
-                                Finalizar
-                            </a>
-                            <a onClick="apagar(this)" href="#" class="btn btn-danger" title="Apagar Tarefa">
-                                <i class="bi bi-trash"></i>
-                                Apagar
-                            </a>
-                            </div>
-                        </div> <!-- Card -->
-                    </div> <!-- Col -->
-            </div> <!-- Container -->
+
+function createCard(tarefa) {
+  return `
+        <div class="col-lg-3 col-md-6 col-12">
+            <div class="card">
+                <div class="card-header">
+                    ${tarefa.titulo}
+                </div>
+                <div class="card-body">
+                    <p class="card-text">${tarefa.descricao}</p>
+                    <p>
+                        <span class="badge text-bg-warning">${tarefa.categoria}</span>
+                    </p>
+                    <a href="#" class="btn btn-success" title="Marcar como Concluída">
+                        <i class="bi bi-check-lg"></i>
+                        Finalizar
+                    </a>
+                    <a onClick="apagar(this)" href="#" class="btn btn-danger" title="Apagar Tarefa">
+                        <i class="bi bi-trash"></i>
+                        Apagar
+                    </a>
+                </div>
+            </div> <!-- Card -->
+        </div><!-- Col -->
+
     ` //template literals
-}}
+}
